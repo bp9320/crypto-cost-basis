2701d777356eee7fd7a7720233d346a50548a489
Brian Pirot <brian.pirot@sunquestinfo.com>
2020-02-19 15:42:27 -0600

    roughed out input form, added title and disclaimer, added form component state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React, { fragment } from 'react';
+import TransactionForm from './components/inputForm/TransactionForm';
 // import './App.css';
 
 const App = () => {
   return (
     <fragment>
-      <h1>This is my app</h1>
+      <h1 className='center-align'>Crypto Capital Gains Calculator</h1>
+      <div className='container'>
+        <p className='flow-text center'>
+          This app was built mainly to get practice designing and building React
+          apps. I am not an accountant and make no guarantees that the
+          calculations performed within this app are accurate. I encourage you
+          to seek the help of an accountant or tax expert with any questions
+          regarding tax obligations for crypto transactions.
+        </p>
+      </div>
+      <div className='divider'></div>
+      <TransactionForm />
     </fragment>
   );
 };
--- src/components/inputForm/TransactionForm.js
+++ src/components/inputForm/TransactionForm.js
@@ -1,7 +1,128 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const TransactionForm = () => {
-  return <form></form>;
+  useEffect(() => {
+    const datePicker = document.querySelector('.datepicker');
+    // eslint-disable-next-line no-undef
+    M.Datepicker.init(datePicker, {
+      autoClose: true
+    });
+
+    const select = document.querySelector('#type');
+    // eslint-disable-next-line no-undef
+    M.FormSelect.init(select);
+  });
+
+  const [transaction, setTransaction] = useState({
+    service: '',
+    type: 'sale',
+    asset: '',
+    transdate: '',
+    qty: '',
+    amount: '',
+    fee: ''
+  });
+
+  const { service, type, asset, transdate, qty, amount, fee } = transaction;
+
+  const onChange = e => {
+    setTransaction({ ...transaction, [e.target.name]: e.target.value });
+  };
+
+  return (
+    <div className='row section'>
+      <form action='' className='col s12'>
+        <div className='row'>
+          <div className='input-field col s2'>
+            <input
+              type='text'
+              name='service'
+              id='service'
+              value={service}
+              placeholder='e.g. Coinbase'
+              className='validate'
+              onChange={onChange}
+            />
+            <label htmlFor='service'>Trading Service</label>
+          </div>
+          <div className='input-field col s2'>
+            <select name='type' id='type' value={type} onChange={onChange}>
+              <option value='purchase'>Purchase</option>
+              <option value='sale'>Sale</option>
+            </select>
+            <label htmlFor='type'>Transaction Type</label>
+          </div>
+          <div className='input-field col s1'>
+            <input
+              type='text'
+              name='asset'
+              id='asset'
+              placeholder='e.g. BTC, ETH, LTC'
+              className='validate'
+              value={asset}
+              onChange={onChange}
+            />
+            <label htmlFor='asset'>Asset</label>
+          </div>
+          <div className='input-field col s2'>
+            <input
+              type='text'
+              name='transdate'
+              id='transdate'
+              placeholder='Click to pick date'
+              className='datepicker'
+              value={transdate}
+              onChange={onChange}
+            />
+            <label htmlFor='transdate'>Transaction Date</label>
+          </div>
+          <div className='input-field col s1'>
+            <input
+              type='text'
+              name='qty'
+              id='qty'
+              placeholder='Quantity'
+              value={qty}
+              onChange={onChange}
+            />
+            <label htmlFor='qty'>Quantity</label>
+          </div>
+          <div className='input-field col s2'>
+            <input
+              type='text'
+              name='amount'
+              id='amount'
+              placeholder='Dollar Amount'
+              value={amount}
+              onChange={onChange}
+            />
+            <label htmlFor='amount'>Transaction Amount</label>
+          </div>
+          <div className='input-field col s2'>
+            <input
+              type='text'
+              name='fee'
+              id='fee'
+              placeholder='Fees'
+              value={fee}
+              onChange={onChange}
+            />
+            <label htmlFor='fee'>Transaction Fee</label>
+          </div>
+        </div>
+        <div className='row center-align'>
+          <button
+            className='btn waves-effect waves-light'
+            type='submit'
+            name='submit'
+          >
+            Add Transaction
+            <i className='material-icons right'>send</i>
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default TransactionForm;
