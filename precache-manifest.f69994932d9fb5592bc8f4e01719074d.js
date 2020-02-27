2957a57d26cc3578279ef97832530303fd65ba18
Brian Pirot <brian.pirot@sunquestinfo.com>
2020-02-26 19:01:13 -0600

    Updates

---
+++ precache-manifest.f69994932d9fb5592bc8f4e01719074d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9be03765927301790c6142ae322b9d2",
+    "url": "/crypto-cost-basis/index.html"
+  },
+  {
+    "revision": "461a8228d1f465aadd46",
+    "url": "/crypto-cost-basis/static/js/2.7b22efc9.chunk.js"
+  },
+  {
+    "revision": "379b712fd8238e18b8c66df506081c41",
+    "url": "/crypto-cost-basis/static/js/2.7b22efc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe67ff9b9fedb1f720fa",
+    "url": "/crypto-cost-basis/static/js/main.33f5bbf3.chunk.js"
+  },
+  {
+    "revision": "59b3db85144f6c6a01f4",
+    "url": "/crypto-cost-basis/static/js/runtime-main.1baa157b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/crypto-cost-basis/precache-manifest.28b4e6da2e9d21c439031d99e7057206.js"
+  "/crypto-cost-basis/precache-manifest.f69994932d9fb5592bc8f4e01719074d.js"
 );
 
 self.addEventListener('message', (event) => {
