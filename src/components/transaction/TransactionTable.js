051e54db692cac4859878bc8045ad2e01c3b9bf0
Brian <piratewebdev@gmail.com>
2020-07-21 22:44:12 -0500

    Show error when asset input is invalid

--- src/components/transaction/TransactionTable.js
+++ src/components/transaction/TransactionTable.js
@@ -29,6 +29,8 @@ const TransactionTable = () => {
     for (let transaction of transactions) {
       if (!validations.isAlphanumeric(transaction.service)) {
         return transaction.displayDate;
+      } else if (!validations.isAlphanumeric(transaction.asset)) {
+        return transaction.displayDate;
       } else if (!transaction.transDate.isValid()) {
         return transaction.displayDate;
       }
